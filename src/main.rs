7ebfe978059ae0d8634e76751795b75da9d368d9
Lev1ty <adamxhy123@outlook.com>
2025-03-07 21:02:28 -0500

    log to file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use text_language_server::{Error, ServerBuilder};
 use tokio::io::{stdin, stdout};
 use tower_lsp::{LspService, Server};
+use tracing_appender::{non_blocking, rolling};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+  let (writer, _guard) = non_blocking(rolling::never("/tmp", "text-language-server.log"));
   tracing_subscriber::fmt()
     .with_max_level(tracing::Level::DEBUG)
+    .with_writer(writer)
     .try_init()
     .map_err(Error::TracingSubscriberInit)?;
   let (service, socket) =
